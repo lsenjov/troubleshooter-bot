2a5a9434a0dfc80c0b3840497a94320fd2cf5653
Logan Senjov <logansenjov@gmail.com>
2019-05-25 11:08:22 +1000

    Added from command

---
+++ src/troubleshooter_bot/extensions/from.clj
@@ -0,0 +1,40 @@
+(ns troubleshooter-bot.extensions.from
+  (:require [discord.bot :as bot]
+            [clojure.pprint :refer [pprint]]
+            [taoensso.timbre :as timbre]
+            )
+  )
+
+
+(defn- from*
+  [client {:keys [content] :as message}]
+  (try
+  (let [[from-val from-type _ to-type :as tokens] (-> content (clojure.string/split #"\s+"))
+        _ (if (not (= 4 (count tokens))) (throw (Exception. "Invalid input string. `!help from`")))
+        ; Abuse units to do the heavy lifting
+        ret (clojure.java.shell/sh "units" (str from-val from-type) to-type)
+        ]
+    (timbre/debug ret)
+    (if (zero? (:exit ret))
+      (-> ret
+          :out
+          (clojure.string/split #"\n")
+          first
+          (clojure.string/split #"\s")
+          last
+          (str " " to-type)
+          bot/say
+          )
+      (-> ret
+          :out
+          (str "`help from` for more details")
+          bot/say
+          )
+      ))
+  (catch Exception e (bot/say (.getMessage e)))))
+(bot/defcommand from
+  [client message]
+  "Converts units from one to another.
+  Format example: from 10 cm to m"
+  (from* client message)
+  )
